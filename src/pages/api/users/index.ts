414dd784d87782875f5f20e7c872c05c493c395c
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2022-03-29 22:25:55 -0300

    feat: arquivos mockados de exemplo para listagem de users no front-end

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 // Rota do Backend do Next
 // Prém sempre se lembrar que nossa logica de pagamento, senha, autenticação deve ser feita sempre no back-end, para não abrir questões de segurança.
 // API Routes, executadas como Serverlless, não existe um servidor express por exemplo, mas toda vez q a rota é chamada, ele vai chamar um ambiente isolado e executar a função, a partir do momento que ela devolver uma resposta, ela encerra a execução do ambiente.
