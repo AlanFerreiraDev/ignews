e21ed14be3d4de9b1639c7934a4583f2d6ec5394
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-04-28 18:09:20 -0300

    refactor: adicionar conteudo e funcao de busca por dados no server next (estatico)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,64 @@
-export default function Home() {
-  return <h1>Helo World</h1>
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+
+import styles from './home.module.scss'
+import { stripe } from '@/services/stripe'
+import { priceFormattedInDollars } from '@/utils/priceFormatted'
+import { SubscribeButton } from '@/components'
+
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            get access to all the publications <br />
+            <span>for {product.amount}/month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve(
+    String(process.env.STRIPE_PRODUCT_KEY)
+  )
+
+  const { id, unit_amount } = price
+  const unitAmountNumber = Number(unit_amount)
+  const timeOf24Hours = 60 * 60 * 24
+
+  const priceFormatted = priceFormattedInDollars(unitAmountNumber)
+
+  const product = {
+    priceId: id,
+    amount: priceFormatted,
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: timeOf24Hours,
+  }
 }
