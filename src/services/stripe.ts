d155c2b02742ec4f7f7c5e1ee148378e8eefebd8
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-04-28 18:09:50 -0300

    feat: criar chamada para a API do stripe

---
+++ src/services/stripe.ts
@@ -0,0 +1,9 @@
+import Stripe from 'stripe'
+
+export const stripe = new Stripe(String(process.env.STRIPE_API_KEY), {
+  apiVersion: '2022-11-15',
+  appInfo: {
+    name: 'ignews',
+    version: '1.0.0',
+  },
+})
