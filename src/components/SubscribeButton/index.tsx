a08d8fda8407dd15ecbdb060f2bac85cd35dec43
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-05-01 14:55:12 -0300

    refactor: criar de subscribe e redirecionamento para o checkout do stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,41 @@
+import { signIn, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
+import { api } from '@/services/api'
+import { getStripeJs } from '@/services/stripe-js'
 
 interface ISubscribeButtonProps {
   priceId: string
 }
 
 export const SubscribeButton = ({ priceId }: ISubscribeButtonProps) => {
+  const { data: session } = useSession()
+  async function handleSubscribe() {
+    const userHasNoActiveSession = !session
+
+    if (userHasNoActiveSession) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const callStripeSubscription = await api.post('/subscribe')
+
+      const { sessionId } = callStripeSubscription.data
+
+      const stripeService = await getStripeJs()
+
+      await stripeService?.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error)
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   )
