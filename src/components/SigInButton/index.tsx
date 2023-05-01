4ca4be6f64b7eace2b7f9bde3b28e1a7b03a2c2e
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-05-01 14:53:42 -0300

    fix: corrigir erro de reidratacao do react/next no client e server

--- src/components/SigInButton/index.tsx
+++ src/components/SigInButton/index.tsx
@@ -1,20 +1,52 @@
-import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+
+import { signIn, signOut, useSession, GetSessionParams } from 'next-auth/react'
+
 import styles from './styles.module.scss'
+import { GithubLogo } from '@phosphor-icons/react'
+import { useEffect, useState } from 'react'
 
 export const SigInButton = () => {
-  const isUserLoggedIn = true
+  const [colorButton, setColorButton] = useState<string>('')
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.sigInButton}>
-      <FaGithub color="#04d361" />
-      Alan Ferreira
+  const handleSignIn = async (provider: string) => {
+    await signIn(provider)
+  }
+
+  const handleSignOut = async () => {
+    await signOut()
+  }
+
+  useEffect(() => {
+    const loggedColorButton = '#04d361'
+    const loggedOutColorButton = '#eba417'
+
+    if (session?.user) {
+      setColorButton(loggedColorButton)
+    } else {
+      setColorButton(loggedOutColorButton)
+    }
+  }, [session])
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.sigInButton}
+      onClick={handleSignOut}
+    >
+      <GithubLogo color={colorButton} />
+      {session.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.sigInButton}>
-      <FaGithub color="#eba417" />
-      Sign in with GitHub
+    <button
+      type="button"
+      className={styles.sigInButton}
+      onClick={() => handleSignIn('github')}
+    >
+      <GithubLogo color={colorButton} />
+      Sign in with Github
     </button>
   )
 }
