0d1a605985cc316cb523e6f5b045abefdcecd7b2
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-05-03 12:43:57 -0300

    refactor: inserir componente de ActiveLink com logica para escolha de link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import { SigInButton } from '../SigInButton'
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
 
 export const Header = () => {
   return (
@@ -7,10 +9,12 @@ export const Header = () => {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">
+          <ActiveLink href="/" activeClassName={styles.active}>
             Home
-          </a>
-          <a href="">Posts</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+            Posts
+          </ActiveLink>
         </nav>
         <SigInButton />
       </div>
